528bc3aceb023df06ee3475bf1c9a94f5b0b85ae
Ondřej Hošek <ondra.hosek@gmail.com>
2024-03-31 22:48:06 +0200

    add initial code (autogenerated stm32g0b0)

---
+++ code/on_board/stm32g0b0/regenerate.ps1
@@ -0,0 +1,18 @@
+If (-not (Test-Path -Path ".\STM32G0B0.svd")) {
+    Throw "Wrong path, please move to directory containing STM32G0B0.svd."
+}
+
+# remove current generated code
+Remove-Item -Recurse -Force -Path "src"
+
+# generate fresh code
+svd2rust -i ".\STM32G0B0.svd" -o .
+
+# beautify
+form -i ".\lib.rs" -o "src"
+
+# remove lib.rs intermediate artifact
+Remove-Item -Path ".\lib.rs"
+
+# beautify harder
+cargo fmt
---
+++ code/on_board/stm32g0b0/regenerate.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+if [ ! -f "./STM32G0B0.svd" ]
+then
+    echo "Wrong path, please move to directory containing STM32G0B0.svd." >&2
+    exit 1
+fi
+
+# remove current generated code
+rm -rf "src"
+
+# generate fresh code
+svd2rust -i "./STM32G0B0.svd" -o .
+
+# beautify
+form -i "./lib.rs" -o "src"
+
+# remove lib.rs intermediate artifact
+rm "./lib.rs"
+
+# beautify harder
+cargo fmt
