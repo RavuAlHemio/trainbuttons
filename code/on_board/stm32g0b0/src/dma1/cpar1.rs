89926002b0866e98a0ce26e077898163659258a3
Ondřej Hošek <ondra.hosek@gmail.com>
2024-10-24 22:36:40 +0200

    stm32g0b0: make DMAx.CPARx.PA settable to direct values

--- code/on_board/stm32g0b0/src/dma1/cpar1.rs
+++ code/on_board/stm32g0b0/src/dma1/cpar1.rs
@@ -9,7 +9,7 @@ pub type PaR = crate::FieldReader<u32>;
 #[doc = "Field `PA` writer - peripheral address It contains the base address of the peripheral data register from/to which the data will be read/written. When PSIZE\\[1:0\\]=01 (16 bits), bit 0 of PA\\[31:0\\]
 is ignored. Access is automatically aligned to a half-word address. When PSIZE=10 (32 bits), bits 1 and 0 of PA\\[31:0\\]
 are ignored. Access is automatically aligned to a word address. In memory-to-memory mode, this register identifies the memory destination address if DIR=1 and the memory source address if DIR=0. In peripheral-to-peripheral mode, this register identifies the peripheral destination address DIR=1 and the peripheral source address if DIR=0. Note: this register is set and cleared by software. It must not be written when the channel is enabled (EN = 1). It is not read-only when the channel is enabled (EN=1)."]
-pub type PaW<'a, REG> = crate::FieldWriter<'a, REG, 32, u32>;
+pub type PaW<'a, REG> = crate::FieldWriter<'a, REG, 32, u32, crate::Safe>;
 impl R {
     #[doc = "Bits 0:31 - peripheral address It contains the base address of the peripheral data register from/to which the data will be read/written. When PSIZE\\[1:0\\]=01 (16 bits), bit 0 of PA\\[31:0\\]
 is ignored. Access is automatically aligned to a half-word address. When PSIZE=10 (32 bits), bits 1 and 0 of PA\\[31:0\\]
@@ -38,7 +38,7 @@ impl crate::RegisterSpec for Cpar1Spec {
 impl crate::Readable for Cpar1Spec {}
 #[doc = "`write(|w| ..)` method takes [`cpar1::W`](W) writer structure"]
 impl crate::Writable for Cpar1Spec {
-    type Safety = crate::Unsafe;
+    type Safety = crate::Safe;
     const ZERO_TO_MODIFY_FIELDS_BITMAP: u32 = 0;
     const ONE_TO_MODIFY_FIELDS_BITMAP: u32 = 0;
 }
